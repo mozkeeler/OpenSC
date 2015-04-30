mozkeeler/OpenSC

commit 643080baf9ffd404c88cb99493668eacfff3ab5e

honour key capabilities for decryption/verification
fixes #419

src/pkcs11/mechanism.c
@@ -609,6 +609,18 @@ sc_pkcs11_verify_init(sc_pkcs11_operation_t *operation,
 	data->info = NULL;
 	data->key = key;
 
+	if (key->ops->can_do)   {
+		rv = key->ops->can_do(operation->session, key, operation->type->mech, CKF_SIGN);
+		if (rv == CKR_OK)   {
+			/* Mechanism recognised and can be performed by pkcs#15 card */
+		}
+		else  {
+			/* Mechanism cannot be performed by pkcs#15 card, or some general error. */
+			free(data);
+			LOG_FUNC_RETURN(context, rv);
+		}
+	}
+
 	/* If this is a verify with hash operation, set up the
 	 * hash operation */
 	info = (struct hash_signature_info *) operation->type->mech_data;
@@ -869,11 +881,24 @@ sc_pkcs11_decrypt_init(sc_pkcs11_operation_t *operation,
 			struct sc_pkcs11_object *key)
 {
 	struct signature_data *data;
+	CK_RV rv;
 
 	if (!(data = calloc(1, sizeof(*data))))
 		return CKR_HOST_MEMORY;
 
 	data->key = key;
+
+	if (key->ops->can_do)   {
+		rv = key->ops->can_do(operation->session, key, operation->type->mech, CKF_DECRYPT);
+		if (rv == CKR_OK)   {
+			/* Mechanism recognised and can be performed by pkcs#15 card */
+		}
+	   	else  {
+			/* Mechanism cannot be performed by pkcs#15 card, or some general error. */
+			free(data);
+			LOG_FUNC_RETURN(context, rv);
+		}
+	}
 
 	operation->priv_data = data;
 	return CKR_OK;

commit 741ac86e8faa3adad2685d589b2036e45978fc6e

pkcs11: fixed broken C_Decrypt
Fixed broken C_Decrypt from 643080baf9ffd404c88cb99493668eacfff3ab5e

fix #449

src/pkcs11/mechanism.c
@@ -593,7 +593,7 @@ done:
 }
 
 /*
- * Initialize a signature operation
+ * Initialize a verify operation
  */
 static CK_RV
 sc_pkcs11_verify_init(sc_pkcs11_operation_t *operation,
@@ -611,10 +611,10 @@ sc_pkcs11_verify_init(sc_pkcs11_operation_t *operation,
 
 	if (key->ops->can_do)   {
 		rv = key->ops->can_do(operation->session, key, operation->type->mech, CKF_SIGN);
-		if (rv == CKR_OK)   {
-			/* Mechanism recognised and can be performed by pkcs#15 card */
+		if ((rv == CKR_OK) || (rv == CKR_FUNCTION_NOT_SUPPORTED))   {
+			/* Mechanism recognized and can be performed by pkcs#15 card or algorithm references not supported */
 		}
-		else  {
+		else {
 			/* Mechanism cannot be performed by pkcs#15 card, or some general error. */
 			free(data);
 			LOG_FUNC_RETURN(context, rv);
@@ -874,7 +874,7 @@ out:
 
 
 /*
- * Initialize a signature operation
+ * Initialize a decrypt operation
  */
 static CK_RV
 sc_pkcs11_decrypt_init(sc_pkcs11_operation_t *operation,
@@ -890,10 +890,10 @@ sc_pkcs11_decrypt_init(sc_pkcs11_operation_t *operation,
 
 	if (key->ops->can_do)   {
 		rv = key->ops->can_do(operation->session, key, operation->type->mech, CKF_DECRYPT);
-		if (rv == CKR_OK)   {
-			/* Mechanism recognised and can be performed by pkcs#15 card */
+		if ((rv == CKR_OK) || (rv == CKR_FUNCTION_NOT_SUPPORTED))   {
+			/* Mechanism recognized and can be performed by pkcs#15 card or algorithm references not supported */
 		}
-	   	else  {
+		else {
 			/* Mechanism cannot be performed by pkcs#15 card, or some general error. */
 			free(data);
 			LOG_FUNC_RETURN(context, rv);